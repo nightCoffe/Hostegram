c4291ec712237ddc5d0fb21ed4a2ce657e2ed598
Your Name <youremail@domain.com>
2022-02-13 13:07:10 -0800

    added RemoteDriver

--- src/test/java/drivers/RemoteDriver.java
+++ src/test/java/drivers/RemoteDriver.java
@@ -10,7 +10,7 @@ import java.net.URL;
 
 import static config.AppRemote.userRemoteConfig;
 
-/*public class RemoteDriver implements WebDriverProvider {
+public class RemoteDriver implements WebDriverProvider {
     public static URL getRemoteUrl() {
         try {
             return new URL(
@@ -28,4 +28,4 @@ import static config.AppRemote.userRemoteConfig;
     }
 }
 
- */
+
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,6 +2,7 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import drivers.RemoteDriver;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
@@ -9,8 +10,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.open;
-import static config.AppRemote.userRemoteConfig;
-import static java.lang.String.format;
 
 
 public class TestBase {
@@ -21,8 +20,7 @@ public class TestBase {
     @BeforeAll
     static void setup() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.remote = format("https://%s:%s@%s", userRemoteConfig.remoteLogin(), userRemoteConfig.remotePassword(),
-                System.getProperty("remoteBrowser"));
+        Configuration.browser = RemoteDriver.class.getName();
         Configuration.startMaximized = true;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
