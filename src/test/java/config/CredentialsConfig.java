f5a5e3571a05793e23a5ec2a4e68b3a3ff8c7d74
Your Name <youremail@domain.com>
2022-02-14 11:01:54 -0800

    added beautiful

--- src/test/java/config/CredentialsConfig.java
+++ src/test/java/config/CredentialsConfig.java
@@ -3,7 +3,8 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({"classpath:credentials.properties"})
-public interface CredentialsConfig extends Config{
+public interface CredentialsConfig extends Config {
     String login();
+
     String password();
 }
