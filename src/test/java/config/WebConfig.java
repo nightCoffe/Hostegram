0ea36637eef518d9d245fe02a005556c9877d53d
Your Name <youremail@domain.com>
2022-02-18 00:56:37 -0800

    fix

---
+++ src/test/java/config/WebConfig.java
@@ -0,0 +1,20 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+
+@Config.Sources("classpath:${environment}.properties")
+public interface WebConfig extends Config {
+
+    @Key("browser")
+    @DefaultValue("CHROME")
+    String browser();
+
+    @Key("browserVersion")
+    @DefaultValue("96.0")
+    String browserVersion();
+
+    @Key("browserSize")
+    @DefaultValue("1920x1080")
+    String browserSize();
+}
