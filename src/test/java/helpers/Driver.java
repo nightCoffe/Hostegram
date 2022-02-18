86b46d60181330415e37d2a539c4e8543261d15e
Your Name <youremail@domain.com>
2022-02-18 06:09:04 -0800

    fix

--- src/test/java/helpers/Driver.java
+++ src/test/java/helpers/Driver.java
@@ -10,10 +10,7 @@ import static tests.TestBase.credentials;
 public class Driver {
     public static void configure() {
 
-        String selenoidUrl = credentials.URL();
-        String login = credentials.login();
-        String password = credentials.password();
-        Configuration.remote = format("https://%s:%s@", login, password, selenoidUrl);
+        Configuration.remote = format("https://%s:%s@%s", credentials.login(), credentials.password(), System.getProperty("remoteBrowser"));
         Configuration.browser = AppConfig.config.browser();
         Configuration.browserVersion = AppConfig.config.browserVersion();
         Configuration.browserSize = AppConfig.config.browserSize();
