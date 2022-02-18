449123ed39fac572046f6abb169bc24a3b57a53b
Your Name <youremail@domain.com>
2022-02-18 10:13:13 -0800

    added firefox.properties

--- src/test/java/config/ProjectConfig.java
+++ src/test/java/config/ProjectConfig.java
@@ -5,8 +5,8 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:local.properties",
-        "classpath:remote.properties",
+        "classpath:chrome.properties",
+        "classpath:firefox.properties",
         "classpath:credentials.properties"
 })
 
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,20 +1,15 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-//import config.CredentialsConfig;
 import config.ProjectConfig;
-//import config.WebConfig;
 import helpers.Attach;
 import helpers.Driver;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.open;
-import static java.lang.String.format;
 
 
 public class TestBase {
@@ -33,30 +28,6 @@ public class TestBase {
 
         Driver.configure();
     }
-
-
-/*    public static CredentialsConfig credentials =
-            ConfigFactory.create(CredentialsConfig.class);
-
-    public static WebConfig webConfig = ConfigFactory.create(WebConfig.class, System.getProperties());
-
-    @BeforeAll
-    static void setup() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.startMaximized = true;
-        Configuration.browser = webConfig.browser();
-        Configuration.browserVersion = webConfig.browserVersion();
-        Configuration.browserSize = webConfig.browserSize();
-        Configuration.remote = format("https://%s:%s@%s", credentials.login(), credentials.password(),
-                System.getProperty(credentials.remoteBrowser()));
-        Configuration.timeout = 10000;
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
-    }
-
- */
 
     @AfterEach
     public void tearDown() {
